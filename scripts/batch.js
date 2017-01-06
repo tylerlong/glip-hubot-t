176b397be86f47460a58aabe675338f562fb3ece
Tyler Long <tyler4long@gmail.com>
2017-01-06 17:30:21 +0800

    Standard JS style

--- scripts/batch.js
+++ scripts/batch.js
@@ -5,19 +5,19 @@
 //   hubot send to group <group_id> <message> <n> times
 //   hubot group_id - show the current group ID
 
-module.exports = function(robot) {
-  robot.respond(/send to group\s+(\d+)\s+([\s\S]+)\s+(\d+)\s+times$/, function(res) {
-    const times = parseInt(res.match[3]);
+module.exports =  (robot) => {
+  robot.respond(/send to group\s+(\d+)\s+([\s\S]+)\s+(\d+)\s+times$/, (res) => {
+    const times = parseInt(res.match[3])
     const envelope = {
       user: {
         reply_to: parseInt(res.match[1])
       }
-    };
-    for (i = 0; i < times; i++) {
+    }
+    for (let i = 0; i < times; i++) {
       robot.send(envelope, res.match[2].replace(/<n>/, i + 1))
     }
-  });
-  robot.respond(/group_id$/, function(res) {
-    res.send(res.envelope.room);
-  });
-};
+  })
+  robot.respond(/group_id$/, (res) => {
+    res.send(res.envelope.room)
+  })
+}
